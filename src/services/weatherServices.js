dd08e0a854f7a2805270fd3ffba26b45cc9ebf55
amazingtoludada <128092606+amazingtoludada@users.noreply.github.com>
2025-01-08 22:24:53 -0400

    Update weatherServices.js

--- src/services/weatherServices.js
+++ src/services/weatherServices.js
@@ -1,4 +1,4 @@
-const apiKey = 'cc66a046f77819d083da52f9e96c56c8';
+const apiKey = '[INSERT API KEY HERE]';
 const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
 function fetchWeather(city) {
